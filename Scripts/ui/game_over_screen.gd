44f261d2f70abeed6359fd55f72c5dcc0f417d58
bea314 <beatrizdelpinal98@gmail.com>
2026-04-21 17:46:14 -0600

    implementacion de SFXS de menu GAME OVER

--- Scripts/ui/game_over_screen.gd
+++ Scripts/ui/game_over_screen.gd
@@ -3,10 +3,21 @@ extends CanvasLayer
 const LEVEL_PATH := "res://Ecenes/level.tscn"
 const MENU_PATH := "res://Ecenes/Menu/Menu.tscn"
 
+const _SFX_SCREEN_OPEN := preload("res://Recursos/Sound/SFXS/UX_GAME_OVER/GAME_OVER_SCREEN_OPEN.ogg")
+const _SFX_SELECT_RETRY := preload("res://Recursos/Sound/SFXS/UX_GAME_OVER/GAME_OVER_SELECT_RETRY.ogg")
+const _SFX_SELECT_MAIN_MENU := preload("res://Recursos/Sound/SFXS/UX_GAME_OVER/GAME_OVER_SELECT_MAIN_MENU.ogg")
+
+const _HOVER_SFX: Array[AudioStream] = [
+	preload("res://Recursos/Sound/SFXS/UX_GAME_OVER/GAME_OVER_UI_HOVER_01.ogg"),
+	preload("res://Recursos/Sound/SFXS/UX_GAME_OVER/GAME_OVER_UI_HOVER_02.ogg"),
+	preload("res://Recursos/Sound/SFXS/UX_GAME_OVER/GAME_OVER_UI_HOVER_03.ogg"),
+]
+
 @onready var _root: Control = $Root
 @onready var _retry_button: Button = $Root/CenterContainer/MainVBox/RetryButton
 @onready var _menu_button: Button = $Root/CenterContainer/MainVBox/MainMenuButton
 @onready var _audio_music: AudioStreamPlayer = $Audio_Music
+@onready var _audio_sfxs: AudioStreamPlayer = $Audio_Sfxs
 
 
 func _ready() -> void:
@@ -14,6 +25,8 @@ func _ready() -> void:
 	visible = false
 	_retry_button.pressed.connect(_on_retry_pressed)
 	_menu_button.pressed.connect(_on_menu_pressed)
+	_retry_button.mouse_entered.connect(_on_game_over_hover)
+	_menu_button.mouse_entered.connect(_on_game_over_hover)
 
 
 func show_game_over() -> void:
@@ -21,8 +34,18 @@ func show_game_over() -> void:
 		return
 	get_tree().paused = true
 	visible = true
+	_play_sfx(_SFX_SCREEN_OPEN)
 	_audio_music.play()
 	_retry_button.grab_focus()
+
+
+func _play_sfx(stream: AudioStream) -> void:
+	_audio_sfxs.stream = stream
+	_audio_sfxs.play()
+
+
+func _on_game_over_hover() -> void:
+	_play_sfx(_HOVER_SFX.pick_random())
 
 
 func _go_to_scene(path: String) -> void:
@@ -37,8 +60,10 @@ func _go_to_scene(path: String) -> void:
 
 
 func _on_retry_pressed() -> void:
+	_play_sfx(_SFX_SELECT_RETRY)
 	_go_to_scene(LEVEL_PATH)
 
 
 func _on_menu_pressed() -> void:
+	_play_sfx(_SFX_SELECT_MAIN_MENU)
 	_go_to_scene(MENU_PATH)
